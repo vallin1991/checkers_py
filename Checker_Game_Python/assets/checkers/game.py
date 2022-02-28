7094c215dc67df9188b6fd15bab33318aa5e482f
vallin1991 <49724891+vallin1991@users.noreply.github.com>
2022-02-28 13:06:57 -0800

    Game.py
    Added the functions _init, select, _move & change_turn to the class game.py

--- Checker_Game_Python/assets/checkers/game.py
+++ Checker_Game_Python/assets/checkers/game.py
@@ -1,3 +1,5 @@
+from operator import truediv
+from xml.sax.handler import property_lexical_handler
 import pygame
 from .constants import RED, WHITE
 from checkers.board import Board
@@ -5,12 +7,49 @@ from checkers.board import Board
 
 class Game:
     def __init__(self, win):
-        self.selected = None
-        self.board = Board()
-        self.turn = RED
-        self.valid_moves = {}
+        self._init()
         self.win = win
 
     def update(self):
         self.board.draw(self.win)
         pygame.display.update()
+
+    def _init(self):
+
+        self.selected = None
+        self.selected = Board()
+        self.selected = RED
+        self.valid_moves = {}
+
+    def reset(self):
+        self._init()
+
+    def select(self, row, col):
+        if self.selected:
+            result = self._move(row, col)
+            if not result:
+                self.selected = None
+                self.select(row, col)
+        else:
+            piece = self.board.get_piece(row, col)
+            if piece != 0 and piece.color == self.turn:
+                self.selected = piece
+                self.valid_moves = self.board.get_valid_moves(piece)
+                return True
+
+        return False
+
+    def _move(self, row, col):
+        piece = self.board.get_piece(row, col)
+        if self.selected and piece == 0 and (row, col) in self.valid_moves:
+            self.board.move(self.selected, row, col)
+            self.change_turn()
+        else:
+            return False
+        return True
+
+    def change_turn(self):
+        if self.turn == RED:
+            self.turn = WHITE
+        else:
+            self.turn = RED
